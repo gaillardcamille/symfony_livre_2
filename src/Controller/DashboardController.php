91dafff46b279d0723d94f8d7eaa61ed6dc83ffc
gaillardcamille <gaillard.camille48@gmail.com>
2024-02-14 10:46:00 +0100

    Livre crud

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
 	#[Route('/', name: 'app_dashboard')]
-	public function index(): Response
+	public function index(LivreRepository $livreRepository): Response
 	{
 		return $this->render('dashboard/index.html.twig', [
 			'controller_name' => 'DashboardController',
+			'livres' => $livreRepository->findAll(),
 		]);
 	}
 }
---
+++ src/Entity/Livre.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LivreRepository::class)]
+class Livre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Date_de_parution = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): static
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getDateDeParution(): ?\DateTimeInterface
+    {
+        return $this->Date_de_parution;
+    }
+
+    public function setDateDeParution(\DateTimeInterface $Date_de_parution): static
+    {
+        $this->Date_de_parution = $Date_de_parution;
+
+        return $this;
+    }
+}
